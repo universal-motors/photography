d258b4475c0cd4c3712c9878129cf019868dd439
aimanfatima248 <aimanfatima65@outlook.com>
2024-01-15 18:03:23 +0500

    fix: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,114 +1,42 @@
 "use client";
-// import LogoLight from "@/assets/images/logo-light.png";
 import Link from "next/link";
 import { useState } from "react";
+import TrendingFlatIcon from "@mui/icons-material/TrendingFlat";
+import Image from "next/image";
+import BCo from "@/assets/images/bco.png";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 export default function Header() {
   const [isOpen, setMenu] = useState(true);
   const toggleMenu = () => {
     setMenu(!isOpen);
   };
   return (
-    <nav className="navbar bg-black" id="navbar">
-      <div className="container flex flex-wrap items-center justify-end">
-        <a className="navbar-brand text-white" href="index.html">
-          <span className="inline-block dark:hidden">
-            {/* <Image src={LogoDark} className="l-dark" alt="" />
-                        <Image src={LogoLight} className="l-light" alt="" /> */}
-            img
-          </span>
-          {/* <Image
-                        src={LogoLight}
-                        className="hidden dark:inline-block"
-                        alt=""
-                    /> */}
-          img
-        </a>
+    <div className="">
+      <div className=" flex justify-end p-2 bg-[#C7B2A2] font-bold text-[white]  ">
+        Let me guess you built your first website yourself and you've grownout
+        of it.Shop our template collection
+        <TrendingFlatIcon />
+      </div>
+
+      <div className="flex justify-between">
         <div>
-          <Link href={"/"}>
-            <span className="nav-link">Home</span>
-          </Link>
-          <Link href={"/"}>
-            <span className="nav-link">About Us</span>
-          </Link>
-          <Link href={"/"}>
-            <span className="nav-link">Contact Us</span>
-          </Link>
+          <Image className="h-14 mt-6 w-[80px] h-41 p-2" src={BCo} alt="" />
         </div>
-
-        {/* <div className="nav-icons flex items-center lg_992:order-2 ms-auto">
-                    <ul className="list-none menu-social mb-0">
-                        <li className="inline ms-1">
-                            <Link href="#">
-                                <span className="login-btn-primary">
-                                    <span className="btn btn-sm btn-icon rounded-full bg-orange-600 hover:bg-orange-700 border-orange-600 hover:border-orange-700 text-white">
-                                        <i className="uil uil-github"></i>
-                                    </span>
-                                </span>
-                                <span className="login-btn-light">
-                                    <span className="btn btn-sm btn-icon rounded-full bg-gray-50 hover:bg-gray-200 dark:bg-slate-900 dark:hover:bg-gray-700 hover:border-gray-100 dark:border-gray-700 dark:hover:border-gray-700">
-                                        <i className="uil uil-github"></i>
-                                    </span>
-                                </span>
-                            </Link>
-                        </li>
-                        <li className="inline ms-1">
-                            <Link href="#">
-                                <span className="login-btn-primary">
-                                    <span className="btn btn-sm btn-icon rounded-full bg-orange-600 hover:bg-orange-700 border-orange-600 hover:border-orange-700 text-white">
-                                        <i className="uil uil-twitter"></i>
-                                    </span>
-                                </span>
-                                <span className="login-btn-light">
-                                    <span className="btn btn-sm btn-icon rounded-full bg-gray-50 hover:bg-gray-200 dark:bg-slate-900 dark:hover:bg-gray-700 hover:border-gray-100 dark:border-gray-700 dark:hover:border-gray-700">
-                                        <i className="uil uil-twitter"></i>
-                                    </span>
-                                </span>
-                            </Link>
-                        </li>
-                        <li className="inline ms-1">
-                            <Link href="#">
-                                <span className="login-btn-primary">
-                                    <span className="btn btn-sm btn-icon rounded-full bg-orange-600 hover:bg-orange-700 border-orange-600 hover:border-orange-700 text-white">
-                                        <i className="uil uil-instagram"></i>
-                                    </span>
-                                </span>
-                                <span className="login-btn-light">
-                                    <span className="btn btn-sm btn-icon rounded-full bg-gray-50 hover:bg-gray-200 dark:bg-slate-900 dark:hover:bg-gray-700 hover:border-gray-100 dark:border-gray-700 dark:hover:border-gray-700">
-                                        <i className="uil uil-instagram"></i>
-                                    </span>
-                                </span>
-                            </Link>
-                        </li>
-                    </ul>
-                    <button
-                        type="button"
-                        className="collapse-btn inline-flex items-center ms-3 text-dark dark:text-white lg_992:hidden"
-                        onClick={toggleMenu}
-                    >
-                        <span className="sr-only">Navigation Menu</span>
-                        <i >menu</i>
-                    </button>
-                </div> */}
-
-        <div
-          className={`${
-            isOpen === true ? "hidden" : "block"
-          } navigation lg_992:order-1 lg_992:flex`}
-          id="menu-collapse"
-        >
-          <ul className="navbar-nav nav-light" id="navbar-navlist">
-            <Link href={"/"}>
-              <span className="nav-link">Home</span>
-            </Link>
-            <Link href={"/"}>
-              <span className="nav-link">About Us</span>
-            </Link>
-            <Link href={"/"}>
-              <span className="nav-link">Contact Us</span>
-            </Link>
-          </ul>
+        <div className="flex gap-8 px-14 mt-6">
+          <p>Home</p>
+          <p>About</p>
+          <p>Portfolio</p>
+          <p>Services</p>
+          <p className=" text-[8px] lg:text-[16px]">For Designers</p>
+          <p>Tools</p>
+          <p>Contact</p>
+          <p>Blog</p>
+          <div className="border border-black rounded-full h-11 p-4 py-1 text-[12px] w-48 xl:text-[16px]">
+            <button>Shop Templates</button>
+            <ShoppingCartIcon />{" "}
+          </div>
         </div>
       </div>
-    </nav>
+    </div>
   );
 }
